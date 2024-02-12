423617261d3c6892cb5285cddb78a60eecaceb39
Sourabh <81312909+SourabhGPatil@users.noreply.github.com>
2024-02-12 23:53:43 +0530

    Update TestRunner.java

--- Project_1_UI_Automation/Code Files/src/test/java/Runner/TestRunner.java
+++ Project_1_UI_Automation/Code Files/src/test/java/Runner/TestRunner.java
@@ -1,22 +1,19 @@
 package Runner;
 
-
+// Import statements for necessary libraries and annotations
 import org.junit.runner.RunWith;
 import org.testng.annotations.Test;
-
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-
-
+// Annotation to specify TestNG test options for Cucumber
 @CucumberOptions(
-		features = "Features/SignUpPage.feature",
-		glue = "StepDefinitions",
-		tags="@SignUpPage"
+		features = "Features/SignUpPage.feature", // Path to the feature file(s)
+		glue = "StepDefinitions", // Package where step definitions are located
+		tags="@SignUpPage" // Tags to filter scenarios to be executed
 		)
 
-
+// TestRunner class that extends AbstractTestNGCucumberTests to run Cucumber tests with TestNG
 public class TestRunner extends AbstractTestNGCucumberTests {	
 	
 }
